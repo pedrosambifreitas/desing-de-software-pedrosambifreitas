17ff04595c5137dbb8a1ec0735e20953fea08aba
pedrosambifreitas <pedrosambifreitas@hotmail.com>
2015-03-19 08:29:34 -0300

    adicionando placar

--- PROJETO 1.py
+++ PROJETO 1.py
@@ -37,78 +37,96 @@ while jogador<3 and computador <3:
 	elif x==tesoura and y==spock:
 		print("o computador jogou tesoura,voce venceu")
 		jogador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 	elif x==tesoura and y==papel:
 		print("o computador jogou tesoura,Voce perdeu")
 		computador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 	elif x==tesoura and y==lagarto:
 		print("o computador jogou tesoura, voce perdeu")
 		computador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 	elif x==tesoura and y==pedra:
 		print("o computador jogou tesoura, voce venceu")
 		jogador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 
 #computador sendo spock
 	elif x==spock and y==tesoura:
 		print("o computador jogou spock, voce perdeu")
 		computador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 	elif x==spock and y==papel:
 		print("o computador jogou spock, voce venceu")
 		jogador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 	elif x==spock and y==lagarto:
 		print("o computador jogou spock, voce venceu")
 		jogador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 	elif x==spock and y==pedra:
 		print("o computador jogou spock, voce perdeu")
 		computador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 
 #computador sendo papel
 	elif x==papel and y==tesoura:
 		print("o computador jogou papel, voce venceu")
 		jogador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 	elif x==papel and y==spock:
 		print("o computador jogou papel, voce perdeu")
 		computador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 	elif x==papel and y==lagarto:
 		print("o computador jogou papel, voce venceu")
 		jogador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 	elif x==papel and y==pedra:
 		print("o computador jogou papel, voce perdeu")
 		computador+=1
-		
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 #computador sendo lagarto
 	elif x==lagarto and y==tesoura:
 		print("o computador jogou lagarto, voce venceu")
 		jogador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 	elif x==lagarto and y==spock:
 		print("o computador jogou lagarto, voce perdeu")
 		computador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 	elif x==lagarto and y==papel:
 		print("o computador jogou lagarto, voce perdeu")
 		computador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 	elif x==lagarto and y==pedra:
 		print("o computador jogou lagarto, voce venceu")
 		jogador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 		
 #computador sendo pedra
 	elif x==pedra and y==tesoura:
 		print("o computador escolheu pedra, voce perdeu")
 		computador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 	elif x==pedra and y==spock:
 		print("o computador escolheu pedra, voce venceu")
-		jogador+=1	
+		jogador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 	elif x==pedra and y==papel:
 		print("o computador escolheu pedra, voce venceu")
 		jogador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 	elif x==pedra and y==lagarto:
 		print("o computador escolheu pedra, voce perdeu")
 		computador+=1
+		print("Placar : computador %d X %f jogador"%(computador,jogador))
 
 
 if jogador==3:
   print("parabeens nerdão, levou a melhor em cima do Sheldon, ta preparado pra cursar engenharia no Insper!")
 if computador == 3:
-  print("xiii nerdão voce perdeu, volta a assistir o seu anime predileto!")	
-	
+  print("xiii nerdão voce perdeu, volta a assistir o seu anime predileto!")
 
 		
 		
